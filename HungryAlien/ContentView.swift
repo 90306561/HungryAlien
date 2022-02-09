16c3f324b12f45105ffb759e7dd8cb50ae3b1ca3
90306561 <72754329+90306561@users.noreply.github.com>
2022-02-09 10:31:12 -0600

    Initial Commit

---
+++ HungryAlien/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HungryAlien
+//
+//  Created by 90306561 on 2/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HungryAlien/HungryAlienApp.swift
@@ -0,0 +1,17 @@
+//
+//  HungryAlienApp.swift
+//  HungryAlien
+//
+//  Created by 90306561 on 2/9/22.
+//
+
+import SwiftUI
+
+@main
+struct HungryAlienApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
